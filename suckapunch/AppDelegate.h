b1f4cc828293eeccfbfa5e8cc21bcfafbe70640f
Keith Elliott <mrkeithelliott@gmail.com>
2012-12-11 15:39:24 -0500

    Initial Commit

---
+++ suckapunch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  suckapunch
+//
+//  Created by Keith Elliott on 12/11/12.
+//  Copyright (c) 2012 Keith Elliott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ suckapunch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  suckapunch
+//
+//  Created by Keith Elliott on 12/11/12.
+//  Copyright (c) 2012 Keith Elliott. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
